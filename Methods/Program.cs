7b3c65f1f49eef77c43d295e92b03509cc59291a
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-10-23 16:02:41 +0300

    Methods_With_parameters

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -6,10 +6,18 @@
         Add();
         Add();
         Add();
+       var result= Add2(20,30);
+        Console.WriteLine(result);
         Console.ReadLine();
     }
     static void Add()
     {
         Console.WriteLine("Added!!!");
+    }
+    //Parametreli Metotlarla Çalışmak
+    static int Add2(int num1, int num2)
+    {
+        var result =num1+ num2; 
+        return result;
     }
 }
