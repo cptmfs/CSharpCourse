a4fccf832cac1fafee1329e5d1f9688b3e212fae
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-10-23 15:51:02 +0300

    IF_ELSE

---
+++ Conditionals/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var number = 11;
+        if (number == 10)
+            Console.WriteLine("Number is 10");
+        else
+            Console.WriteLine("Number is not 10");
+        Console.ReadLine();
+    }
+}
