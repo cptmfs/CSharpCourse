450007da4705aa50069b1dda5907604449a802ac
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-10-23 16:15:29 +0300

    Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] students = new string[3] { "Ferit", "Seyma", "Sevval" };
+        string[] students2 = { "Ferit", "Seyma", "Sevval" };
+        // students2[3] = "Ali";
+        
+        foreach (var student in students2)
+        {
+            Console.WriteLine(student);
+        }
+        Console.WriteLine();
+        Console.ReadLine();
+    }
+}
