736d86d3587ca41d43c418e18c3c9a548781623e
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-10-23 16:19:16 +0300

    Multidimensional_Arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -2,15 +2,37 @@
 {
     private static void Main(string[] args)
     {
-        string[] students = new string[3] { "Ferit", "Seyma", "Sevval" };
-        string[] students2 = { "Ferit", "Seyma", "Sevval" };
-        // students2[3] = "Ali";
-        
-        foreach (var student in students2)
+        //string[] students = new string[3] { "Ferit", "Seyma", "Sevval" };
+        //string[] students2 = { "Ferit", "Seyma", "Sevval" };
+        //// students2[3] = "Ali";
+
+        //foreach (var student in students2)
+        //{
+        //    Console.WriteLine(student);
+        //}
+        //Console.WriteLine();
+
+        // Çok Boyutlu Diziler
+
+        string[,] regions = new string[5, 3]
         {
-            Console.WriteLine(student);
+            {"Istanbul", "Izmit", "Balikesir" },
+            { "Ankara", "Konya", "Kirikkale" },
+            { "Antalya", "Adana", "Mersin" },
+            {"Rize", "Trabzon", "Samsun" },
+            {"Izmir", "Mugla", "Manisa" }
+            
+        };
+
+        for (int i = 0; i <= regions.GetUpperBound(0); i++)
+        {
+            for (int j = 0; j <= regions.GetUpperBound(1); j++)
+            {
+                Console.WriteLine(regions[i, j]);
+            }
+            Console.WriteLine("**********");
         }
-        Console.WriteLine();
-        Console.ReadLine();
+
+            Console.ReadLine();
     }
 }
