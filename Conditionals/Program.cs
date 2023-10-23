1d419e73070643c08337bd6ae3d474f97b0be97a
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-10-23 15:53:45 +0300

    ELSE_IF

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -2,11 +2,20 @@
 {
     private static void Main(string[] args)
     {
+        // If Bloklarıyla Çalışmaya Başlamak
         var number = 11;
         if (number == 10)
             Console.WriteLine("Number is 10");
+        else if(number==20)
+            Console.WriteLine("Number is 20");
         else
             Console.WriteLine("Number is not 10");
+
+
+        //Single Line If İle Çalışmak
+        // Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
+
+
         Console.ReadLine();
     }
 }
