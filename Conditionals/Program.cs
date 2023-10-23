1e078b884ab97af71fc2231a009e10c6870be704
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-10-23 15:59:52 +0300

    IF_BLOCK

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -3,7 +3,7 @@
     private static void Main(string[] args)
     {
         // If Bloklarıyla Çalışmaya Başlamak
-        var number = 11;
+        var number = 93;
         //if (number == 10)
         //    Console.WriteLine("Number is 10");
         //else if(number==20)
@@ -35,16 +35,32 @@
         // Çoklu Şartlarla Çalışmak
 
 
-        if (number >= 0 && number <= 100)
-            Console.WriteLine("Number is between 0-100");
-        
-        
-        else if (number > 100 && number <= 200)
-            Console.WriteLine("Number is between 101-200");
+        //if (number >= 0 && number <= 100)
+        //    Console.WriteLine("Number is between 0-100");
 
-        else if (number > 200 || number < 0)
-            Console.WriteLine("Number is less than 0 or greater than 200");
 
-        Console.ReadLine();
+        //else if (number > 100 && number <= 200)
+        //    Console.WriteLine("Number is between 101-200");
+
+        //else if (number > 200 || number < 0)
+        //    Console.WriteLine("Number is less than 0 or greater than 200");
+
+
+        // İç İçe If Bloklarıyla Çalışmak
+
+        if (number < 100)
+        {
+            if (number >= 90 && number < 95)
+            {
+                Console.WriteLine("Number is between 90-95");
+            }
+            else
+            {
+                Console.WriteLine("Number is not between 90-95");
+
+            }
+        }
+
+                    Console.ReadLine();
     }
 }
