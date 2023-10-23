ab5e5fc1ebdbcc80cd0f4e8ddd89331e754d2647
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-10-23 15:47:18 +0300

    Enum

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -3,8 +3,7 @@
     private static void Main(string[] args)
     {
         //Value Types
-        //Console.WriteLine("Hello, World!");
-
+        // Console.WriteLine("Hello, World!");
         double number5 = 10.4;
         decimal numbers = 10;
         char character = 'A';
@@ -14,14 +13,19 @@
         int number1 = 2147483647;
         long number2 = 9223372036854775807;
 
-        Console.WriteLine("Number1 is {0]", number1);
+        Console.WriteLine("Number1 is {0}", number1);
         Console.WriteLine("Number2 is {0}", number2);
-        Console.WriteLine("Number3 is (0]", number3);
+        Console.WriteLine("Number3 is {0}", number3);
         Console.WriteLine("Number4 is {0}", number4);
         Console.WriteLine("Number5 is {0}", number5);
-        Console.WriteLine("Character is : {0]", (int)character);
+        Console.WriteLine("Character is : {0}", (int)character);
+        Console.WriteLine((int)Days.Friday);
         Console.ReadLine();
-    }
 
+    }
+    enum Days
+    {
+        Monday = 10, Tuesday = 20, Wednesday = 30, Thursday = 40, Friday , Saturday, Sunday
+    }
 
 }
