e6ec940408019164dbfa0f2314be67a57dfcd447
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-10-23 15:31:10 +0300

    .

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Value Types
+        //Console.WriteLine("Hello, World!");
+
+        double number5 = 10.4;
+        decimal numbers = 10;
+        char character = 'A';
+        bool condition = false;
+        byte number4 = 255;
+        short number3 = 32767;
+        int number1 = 2147483647;
+        long number2 = 9223372036854775807;
+
+        Console.WriteLine("Number1 is {0]", number1);
+        Console.WriteLine("Number2 is {0}", number2);
+        Console.WriteLine("Number3 is (0]", number3);
+        Console.WriteLine("Number4 is {0}", number4);
+        Console.WriteLine("Number5 is {0}", number5);
+        Console.WriteLine("Character is : {0]", (int)character);
+        Console.ReadLine();
+    }
+
+
+}
